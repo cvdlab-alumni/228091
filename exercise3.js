45cc5dfd280eb01c8a16825bff2a8695ff5162aa
Corrado Cardone <corrado.cardone@gmail.com>
2012-04-04 21:43:31 +0300

    Update exercise3.js

---
+++ exercise3.js
@@ -0,0 +1,22 @@
+var pillarsArea1 = SIMPLEX_GRID([([0.15, -7, 0.15]),[0.15, -6, 0.15],[1.5,1,1]]);
+  var floorArea1 = SIMPLEX_GRID([[10],[0.15, 8, 0.15],[-1.5, -1, -1,0.3]]);
+	var pillarsArea2 = SIMPLEX_GRID([REPLICA(3)([0.15, -7, 0.15]),[0.15, -7, 0.15],[1.5,1,1]]);
+	var floorArea2 = SIMPLEX_GRID([[27],[0.15, 12, 0.15],[-1.5, -1, -1,0.3]]);
+	var translatedfloorArea1 = T([0,1])([-1,-1])(floorArea1);
+	var translatedfloorArea2 = T([0,1])([-2.5,-2.5])(floorArea2);
+	//DRAW(pillarsArea1);
+	//DRAW(floorArea1);
+	//DRAW(pillarsArea2);
+	//DRAW(floorArea2);
+	//DRAW(translatedfloorArea2);
+	var building1 = STRUCT([pillarsArea1, translatedfloorArea1]);
+	var building2 = STRUCT([pillarsArea2, translatedfloorArea2]);
+	var translatedBuikding1 = T([0,1])([1,16])(building1);
+	var translatedBuikding2 = T([0,1])([25,7])(building2);
+	var redBuilding1 = COLOR([1,0,0])(translatedBuikding1);
+	DRAW(redBuilding1);
+	var bluBuilding2 = COLOR([0,1,0])(translatedBuikding2);
+	DRAW(bluBuilding2);
+	var building = STRUCT([translatedBuikding1,translatedBuikding2]);	
+	
+	//DRAW(building);
