448a7ead6d467e9bbf0bd37f2957fde8db0a8e6e
Corrado Cardone <corrado.cardone@gmail.com>
2012-04-04 22:13:54 +0300

    Update exercise4.js

--- exercise4.js
+++ exercise4.js
@@ -1,5 +1,5 @@
-var gambeSedia = SIMPLEX_GRID([([0.03, -0.3, 0.03]),[0.03, -0.3, 0.03],[0.25]]);
-  var sedile = SIMPLEX_GRID([[0,36],[0.03, -0.3, 0.03],[-0.25,0.02]]);
+vvar gambeSedia = SIMPLEX_GRID([([0.03, -0.3, 0.03]),[0.03, -0.3, 0.03],[0.25]]);
+	var sedile = SIMPLEX_GRID([[0.36],[0.36],[-0.25,0.02]]);
 	
 	DRAW(gambeSedia);
 	DRAW(sedile);
