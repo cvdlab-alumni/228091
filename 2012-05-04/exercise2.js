9892a1ae827d0d68af9a781e2c125bd02de33600
Corrado Cardone <corrado.cardone@gmail.com>
2012-05-05 15:36:22 +0300

    cambiati piani carlinga

--- 2012-05-04/exercise2.js
+++ 2012-05-04/exercise2.js
@@ -9,13 +9,15 @@
 *lunghezza = 9,9 + 5,7 feet = 15,6 feet = 4.75488 metri
 *altezza 0,30 metri
 */
-var domain1 = INTERVALS(1)(1080);
-var domain2 = DOMAIN([[0,1],[0,1]])([30,50]);
-var puntiTriangoloMotore = [[0,0], [0.35,0.6], [-0.35,0.6],[0,0]];
-var puntiTriangoloPilota = [[0,0,3], [0.35,0.6,3], [-0.35,0.6,3],[0,0,3]];
-var triangoloMotore = POLYLINE(puntiTriangoloMotore);
-var carlingaPilotaEMotore = EXTRUDE([3])(triangoloMotore);
-var carlingaCodaLato1 = POLYLINE([[0,0,3],[0,0,7.75],[0,0.4,7.75],[0.35,0.6,3],[0,0,3]]);
-var carlingaCodaLato2 = POLYLINE([[0,0,3],[0,0,7.75],[0,0.4,7.75],[-0.35,0.6,3],[0,0,3]]);
-var strutturaCarlinga = STRUCT([carlingaPilotaEMotore,carlingaCodaLato1,carlingaCodaLato2]);
-DRAW(COLOR([0,1,0])(strutturaCarlinga));
+
+//var puntiTriangoloMotore = [[0,0,0],[0,0,0.6] [0,0.35,0.6], [0,-0.35,6],[0,0]];
+var triangoloMotore = TRIANGLE_FAN([[0,0,0],[0,0.35,0.6],[0,-0.35,0.6]]);
+var triangoloPilota = T([0])([2.8956])(triangoloMotore);
+var lineaBase = POLYLINE([[0,0,0],[2.9,0,0]]);
+var lineaLato1 = POLYLINE([[0,0.35,0.6],[2.9,0.35,0.6]]);
+var lineaLato2 = POLYLINE([[0,-0.35,0.6],[2.9,-0.35,0.6]]);
+var totCarlingaMotePilota = STRUCT([triangoloMotore,triangoloPilota,lineaBase,lineaLato1,lineaLato2]);
+var carlingaCodaLato1 = POLYLINE([[2.9,0,0],[7.65,0,0],[7.65,0,0.4],[2.9,0.35,0.6],[2.9,0,0]]);
+var carlingaCodaLato2 = POLYLINE([[2.9,0,0],[7.65,0,0],[7.65,0,0.4],[2.9,-0.35,0.6],[2.9,0,0]]);
+var strutturaCarlinga = STRUCT([totCarlingaMotePilota ,carlingaCodaLato1,carlingaCodaLato2])
+DRAW(strutturaCarlinga);
