288e0a73c22c6c1b4094b83f08ab1862c24b2f88
Daitan_II <corrado.cardone@gmail.com>
2012-06-10 23:09:33 +0200

    prove json

---
+++ prove json/oggettiJson/testJson1.js~
@@ -0,0 +1,12 @@
+{"posts": 
+[
+{ 
+"title":"9lessons | Programming Blog", 
+"url":"http://9lessons.blogspot.com" 
+}, 
+{ 
+"title":"jQuery and Ajax Demos Pard - 3", 
+"url":"jquery-and-ajax-best-demos-part-3.html"
+}, 
+]
+}
---
+++ prove json/oggettiJson/testJson2.js
@@ -0,0 +1,18 @@
+[{
+        "position":"1",
+        "category":"A",
+        "title":"Title to first story",
+        "description":"The first story."
+    },
+    {
+        "position":"2",
+        "category":"B",
+        "title":"Title to second story",
+        "description":"The second story"
+    },
+    {
+        "position":"3",
+        "category":"B",
+        "title":"Title to third story",
+        "description":"The third story"
+    }
