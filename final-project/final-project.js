92cdc316299c844cc29efa36ab1f01f457b8ec58
Corrado Cardone <corrado.cardone@gmail.com>
2012-07-08 19:15:59 +0200

    versione 1

--- final-project/final-project.js
+++ final-project/final-project.js
@@ -1,40 +1,54 @@
-var domain1 = INTERVALS(1)(200);
-var domain2 = DOMAIN([[0,1],[0,1]])([100,100]);
+var domain1 = INTERVALS(1)(50);
+var domain2 = DOMAIN([[0,1],[0,1]])([50,50]);
+var domain3 = DOMAIN([[0,1]])([50]);
+var risultatoConversione = new Array();
 
-function daJsonAArray(tipo,scalaXY){
-  this.scalaXY = scalaXY || 1;//imposta scala di default a 1
-  this.tipo = tipo || 1;//imposta oggetto da trasformare  a 1:polygon
-  								//0:polyline 2:Freepol
-  var ritorno =
+function daJsonAArray(scalaXYZ){
+  this.scalaXYZ = scalaXYZ || 1;//imposta scala di default a 1
+  var puntiImgDicom = new Array();//array contentente punti
+  var jqxhr =
   $.getJSON('oggettiJson/DICOM/json_brain.json', function(data) {
-  var fettaDicom = [];  
-  console.log(data);
-   var puntiImgDicom = [];
-   if(tipo == 2){
-  		var ogettoFreepol = data.plugins[2].sets.valArray;//recupera array contenti immagini prese da una slice
-  		console.log(ogettoFreepol);
-  		$.each(ogettoFreepol,function(indiceInterno,figura){//itera su ogettoFreepol
-    		console.log(figura);
-    		if(figura.length > 0 ){
-    			$.each(figura,function(indicePunti,punto){//itera sui punti di ogni disegno di una una slice
-    			console.log(punto);
-    			puntiFigura = [];
-				puntiFigura.push([((punto.x)/scalaXY),((punto.y)/scalaXY),punto.z]);
-  					});
-  				}
-    		fettaDicom.push(puntiFigura);
-  			});
-  		puntiImgDicom.push(fettaDicom)
-  	 	};
-
-  console.log(puntiImgDicom);
- });
-  return ritorno;	
+    //console.log(data);
+ 	var ogettoFreepol = data.plugins[2].sets.valArray;//recupera array contenti immagini prese da una slice
+  		//console.log(ogettoFreepol);
+  		$.each(ogettoFreepol,function(indiceInterno,figure){//itera sulle figure contenute in una slice
+    		//console.log(figure);
+    		if(figure.length > 0  ){//seleziono immagini contententi informazioni
+    			$.each(figure,function(indicePunti,figura){//itera su una figura della slice
+    			//console.log(punto);
+    				if(figura.length > 30){//prendo le figure con piu di 30 punti
+    					var disegno = [];
+    					$.each(figura,function(indicePunti,punti){//itera su insieme di punti di una figura
+    						if(indicePunti % 10 == 0 ){//prendo un punto ogni 10
+							 //$.each(punti,function(indicePunto,punto){//itera sulle coordinate di un punto (x,y,z)	
+									disegno.push([((punti.x)/scalaXYZ),((punti.y)/scalaXYZ),punti.z/scalaXYZ]);
+  								//});
+  								}
+  						});
+  					}
+  					if(disegno != undefined) {
+  						puntiImgDicom.push(disegno);
+  					}
+  				});
+  			}
+    	});
+  //console.log(puntiImgDicom);
+ }).complete(function() { //alert("complete")
+	copiaRisultato(puntiImgDicom); 	
+ 	});
+  //console.log(risultatoConversione);
+  return jqxhr;
 };
+
+function copiaRisultato(risultatoJson){
+	//copio riusultato su altro array 
+ 	 $.each(risultatoJson,function(indiceDisegno,disegno){//itera sulle coordinate di un punto (x,y,z)	
+			risultatoConversione.push(disegno);});	
+	};
 
 function knots (point,par) {
   this.par = par || 0;
-  console.log(par);
+  //console.log(par);
   var k = 2;	
   var m =point.length;
   var n = (m + k + 1);
@@ -67,26 +81,52 @@ function knots (point,par) {
 			knots.push(ultimo);//inserisci 4 volte l'ultimo valore 			
   			}
   };
-  console.log(knots);
+  //console.log(knots);
   return knots;
 };
 
-function creaCurvaNubs(){
-	var knots0 = knots(punti,0);
-	return  NUBS(S0)(2)(knots0)(punti);
+function creaCurvaNubs(puntiCurva){
+	var knots0 = knots(puntiCurva,0);//calcola knots
+	var curva =  NUBS(S0)(2)(knots0)(puntiCurva);//calcola curva NUBS
+	return curva;
 	};
 
 function creaCurveNubs(arrayPunti){
-	var curveNubs = [];
+	var curveNubs = [];//array di ritorno composto da curve NUBS
 	for(var i = 0; i < arrayPunti.length; i++){
 		curveNubs[i] = creaCurvaNubs(arrayPunti[i]);		
 		}
 	return curveNubs;	
 };
-var puntiDICOM = daJsonAArray(2,100);
-var calcolaCurve = creaCurveNubs(puntiDICOM);
-var knotsDICOM = knots(puntiDICOM,1);
-var nubsDICOM = NUBS(S1)(3)(knotsDICOM)(calcolaCurve);
 
-	var model = COLOR([1,0.89,0.76])(MAP(nubsDICOM)(domain2))
-DRAW(model);
+function disegnaContorni(){
+	for(i=0;i<risultatoConversione.length;i++){
+		var pointZ0 = risultatoConversione[i]; 
+		var knots0 = knots(pointZ0);
+		var c0 = NUBS(S0)(2)(knots0)(pointZ0);
+		var curve0 = COLOR([0.71,0.73,0.83])(MAP(c0)(domain1));
+		DRAW(curve0);
+	};
+};
+	
+function disegnaModello(){
+
+	var curveNUBS = creaCurveNubs(risultatoConversione);//array formato da curve
+	var knotsCurveNUBS = knots(risultatoConversione,1);
+	var nubsDICOM = NUBS(S1)(3)(knotsCurveNUBS)(curveNUBS);
+	//console.log(nubsDICOM.length);
+	var model = COLOR([0.48,0.48,0.51])(MAP(nubsDICOM)(domain2));
+	//var model = MAP(nubsDICOM)(domain2);
+	DRAW(model);
+};
+
+function avviaLetturaJson(){
+	var jqxhr = daJsonAArray(100);//carico il modello con scala 1:100
+	jqxhr.complete(function(){ //alert("third complete");	
+	//console.log(risultatoConversione);
+	//console.log(risultatoConversione.length);
+	disegnaContorni();
+	disegnaModello();
+	});
+};
+
